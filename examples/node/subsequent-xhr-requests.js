4dbe4e3bbbb0f4b73b0ee8239cb419d68589f2e9
jsonberry <jason.s.awbrey@gmail.com>
2018-02-03 18:00:58 -0800

    add more notes to the subsequent xhr request example

--- examples/node/subsequent-xhr-requests.js
+++ examples/node/subsequent-xhr-requests.js
@@ -12,10 +12,26 @@ const { from, range } = require('rxjs/create')
 
 const endpoint = 'http://demo.wp-api.org/wp-json/wp/v2/posts'
 
+/**
+ *      This sets up the initial request and the Observable stream
+ *      In the return from the endpoint, the Axios request headers will have x-wp-totalpages,
+ *      which gives us... the total pages of posts ;)
+ */
 const posts$ = Observable.from(axios.get(endpoint))
-        .switchMap(({ headers }) =>
-                Observable.range(1, Number(headers['x-wp-totalpages'])),
-        )
+        /**
+         *     We now know the total number of pages,
+         *     so we'll switch to a new Observable that is just a range of numbers
+         *     We'll start with 1, and end with whatever the total number of pages is
+         *     This gives us a stream of 1--n--n--n... (example: 1, 2, 3, 4...)
+         */
+        .switchMap((
+                { headers }, // using ES6 function header destructuring and arrow functions here
+        ) => Observable.range(1, Number(headers['x-wp-totalpages'])))
+        /**
+         *     We can now paginate through all posts, getting 10/page
+         *     concatMap will fire off a request, waits until it completes, and then fire the next one
+         *     In each subsequent firing, we ask for the next page of posts
+         */
         .concatMap(page =>
                 Observable.from(
                         axios.get(endpoint, {
@@ -26,6 +42,7 @@ const posts$ = Observable.from(axios.get(endpoint))
                 ),
         )
         .subscribe(
-                ({ data }) => console.log(data.length), 
+                // data here is an Array of WordPress Posts, tacking .length shows us how many per page we are getting
+                ({ data }) => console.log(data.length),
                 err => console.log('Oh no, an error!', err),
         )
