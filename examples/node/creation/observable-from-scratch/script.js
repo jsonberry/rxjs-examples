d7e8c6b4638758d31e7e9c6208991dc8d7e35168
jsonberry <jason.s.awbrey@gmail.com>
2018-11-18 15:30:05 -0800

    chore: update observable-from-scratch to use util observer

--- examples/node/creation/observable-from-scratch/script.js
+++ examples/node/creation/observable-from-scratch/script.js
@@ -1,63 +1,65 @@
-const { Observable } = require("rxjs")
-const faker = require("faker")
+const { Observable } = require('rxjs')
+const faker = require('faker')
+const { observer } = require('../../utils')
 
 const num = () => {
-        return new Promise((resolve, reject) => {
-                const num = faker.random.number({max: 10})
-                setTimeout(() => {
-                    if (num % 2 === 0) {
-                        resolve(num)
-                    } else {
-                        reject(new Error("The value was odd!"))
-                    }
-                }, 500)
-        })
+  return new Promise((resolve, reject) => {
+    const num = faker.random.number({ max: 10 })
+    setTimeout(() => {
+      if (num % 2 === 0) {
+        resolve(num)
+      } else {
+        reject(new Error('The value was odd!'))
+      }
+    }, 500)
+  })
 }
 
-// Named this onSubscription because that's the name of the parameter in RxJS
+// Named this Subscriber because that's the name of the parameter in RxJS
 // A more descriptive name might be "producer" here
 // Also the param in the func is the Observer API
 // Inside this producer we sort of make API calls to the Observer API
-const onSubscription = observer => {
-        const intervalId = setInterval(async () => {
-            try {
-                    const number = await num()
-                    if (number < 9) {
-                        console.log('Number was under 9:', number);
-                        observer.next(number)
-                    } else {
-                        console.log('Number was 9 or over:', number);
-                        console.log('TearDown Trigger: Completion')
-                        observer.complete()
-                    }
+const subscriber = _observer => {
+  const intervalId = setInterval(async () => {
+    try {
+      const number = await num()
+      if (number < 9) {
+        console.log('Number was under 9:', number)
+        _observer.next(number)
+      } else {
+        console.log('Number was 9 or over:', number)
+        console.log('TearDown Trigger: Completion')
+        _observer.complete()
+      }
+    } catch (err) {
+      console.log('TearDown Trigger: Error')
+      _observer.error(err)
+    }
+  }, 1000)
 
-            } catch (err) {
-                    console.log('TearDown Trigger: Error')
-                    observer.error(err)
-            }
-        }, 1000)
-
-        return () => {
-            console.log('---------------- TearDownLogic ---------------------')
-            console.log('--------- This was triggered by either: ------------')
-            console.log('--- [ Completion ] - [ Error ] - [ Implicit Unsubscription ] ---')
-            clearInterval(intervalId)
-        }
+  return () => {
+    console.log('')
+    console.log('----------- TearDownLogic Triggered ----------------')
+    console.log('-------------- Clearing Interval -------------------')
+    console.log('')
+    clearInterval(intervalId)
+    console.log('--------- This was triggered by either: ------------')
+    console.log('[ Completion ] / [ Error ] / [ Explicit Unsubscription ]')
+    console.log('')
+    console.log('In the example observable created, we used setInterval')
+    console.log('We use the teardown logic to clear the interval ID')
+  }
 }
 
-const observer = {
-    next: val => console.log(val),
-    error: err => console.log(err),
-    complete: () => console.log("done"),
-}
+const sub$ = new Observable(subscriber).subscribe(
+  observer('Observable from Scratch'),
+)
 
-const sub$ = Observable.create(onSubscription).subscribe(observer)
-
-
-// If you uncomment the setTimeout it will force the script wait for the setTimeout threshold before finally unsubscribing
+// If you uncomment the setTimeout it will force the script to wait for the setTimeout threshold before finally unsubscribing
 // if an error occurs before the threshold, TearDown logic will run, but the script will still run
-// until the setTimeout threshold is met
+// until the setTimeout threshold is met. If the Observable completes or errors out before the explicit unsubscription,
+// the teardown logic won't be ran because it would have already been triggered
 // setTimeout(() => {
-//     console.log('TearDown Trigger: Implicit Unsubscription')
+//     console.log('TearDown Trigger: Explicit Unsubscription')
 //     sub$.unsubscribe()
 // }, 1600);
