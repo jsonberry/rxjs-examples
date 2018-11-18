d9a8130511cddf786bf49afff7b356e201595062
jsonberry <jason.s.awbrey@gmail.com>
2018-11-18 14:48:22 -0800

    chore: update event-bus-with-subjects with pipes

--- examples/http/scripts/event-bus-with-subjects.js
+++ examples/http/scripts/event-bus-with-subjects.js
@@ -7,16 +7,17 @@
  *      Instead of using a global variable to remember the message,
  *      Buffer is doing that for us
  */
-import { dqs, dqsa, dce } from "./utils.js"
-const Observable = Rx.Observable
-const Subject = Rx.Subject
+import { dqs, dqsa, div, p, button } from './utils.js'
+const {Subject, fromEvent} = rxjs
+const {map, buffer, first, pluck} = rxjs.operators
 
 const read$ = new Subject()
 
-const input1 = dqs("#input1")
-const btn1 = dqs("#btn1")
-const inbox1 = dqs("#inbox1")
-const read1 = dqs("#read1")
+const input1 = dqs('#input1')
+const btn1 = dqs('#btn1')
+const inbox1 = dqs('#inbox1')
+const read1 = dqs('#read1')
+
 /**
  *      input1$ is a stream of events from an input
  *      We map the stream of input events into the keys pressed
@@ -26,12 +27,12 @@ const read1 = dqs("#read1")
  *      ---------------- map ---------------
  *      k----e----y----p----r----e----s----s
  */
-const input1$ = Observable.fromEvent(input1, "input").map(({ data }) => data)
+const input1$ = fromEvent(input1, 'input').pipe(pluck('data'))
 
 /**
  *      Just a simple stream of button clicks, only for "button1"
  */
-const btn1Clicks$ = Observable.fromEvent(btn1, "click")
+const btn1Clicks$ = fromEvent(btn1, 'click')
 
 /**
  *      buffer helps up capture all the keys pressed until button1 is clicked
@@ -39,71 +40,70 @@ const btn1Clicks$ = Observable.fromEvent(btn1, "click")
  *      Think of it as:
  *      streamOfValuesToBuffer$.buffer(bufferByThisObservableEmission$)
  */
-const input1Buffer$ = input1$.buffer(btn1Clicks$)
+const input1Buffer$ = input1$.pipe(buffer(btn1Clicks$))
 
-const input2 = dqs("#input2")
-const btn2 = dqs("#btn2")
-const inbox2 = dqs("#inbox2")
-const read2 = dqs("#read2")
-const input2$ = Observable.fromEvent(input2, "input").map(({ data }) => data)
-const btn2Clicks$ = Observable.fromEvent(btn2, "click")
-const input2Buffer$ = input2$.buffer(btn2Clicks$)
+const input2 = dqs('#input2')
+const btn2 = dqs('#btn2')
+const inbox2 = dqs('#inbox2')
+const read2 = dqs('#read2')
+const input2$ = fromEvent(input2, 'input').pipe(pluck('data'))
+const btn2Clicks$ = fromEvent(btn2, 'click')
+const input2Buffer$ = input2$.pipe(buffer(btn2Clicks$))
 
 input1Buffer$.subscribe(params => {
-        input1.value = ""
+  input1.value = ''
+  const messageId = new Date().toISOString()
+  const container = div()
+  const message = p()
+  const btn = button()
+  container.style.border = '1px solid red'
+  container.style.padding = '1rem'
+  container.style.display = 'flex'
+  container.dataset.messageId = messageId
+  message.textContent = params.join('')
+  btn.textContent = 'mark as read'
+  container.appendChild(message)
+  read1.appendChild(container.cloneNode(true))
+  container.appendChild(btn)
+  inbox2.appendChild(container)
 
-        const messageId = new Date().toISOString()
-        const container = dce("div")
-        const message = dce("p")
-        const btn = dce("button")
-        Observable.fromEvent(btn, "click")
-                .first()
-                .subscribe(ev => read$.next(messageId))
-
-        container.style.border = "1px solid red"
-        container.style.padding = "1rem"
-        container.style.display = "flex"
-        container.dataset.messageId = messageId
-        message.textContent = params.join("")
-        btn.textContent = "mark as read"
-
-        container.appendChild(message)
-        const containerClone = container.cloneNode(true)
-        container.appendChild(btn)
-        inbox2.appendChild(container)
-        read1.appendChild(containerClone)
+  /**
+   * Setup a stream of clicks on the "read message" button
+   * Only take the first click, then turn the stream off
+   * Send a signal to the Subject with the message ID
+   */
+  fromEvent(btn, 'click').pipe(
+    first()
+  ).subscribe(_ => read$.next(messageId))
 })
 
 input2Buffer$.subscribe(params => {
-        input2.value = ""
+  input2.value = ''
+  const messageId = new Date().toISOString()
+  const container = div()
+  const message = p()
+  const btn = button()
+  container.style.border = '1px solid red'
+  container.style.padding = '1rem'
+  container.style.display = 'flex'
+  container.dataset.messageId = messageId
+  message.textContent = params.join('')
+  btn.textContent = 'mark as read'
+  container.appendChild(message)
+  read2.appendChild(container.cloneNode(true))
+  container.appendChild(btn)
+  inbox1.appendChild(container)
 
-        const messageId = new Date().toISOString()
-        const container = dce("div")
-        const message = dce("p")
-        const btn = dce("button")
-        Observable.fromEvent(btn, "click")
-                .first()
-                .subscribe(ev => read$.next(messageId))
-
-        container.style.border = "1px solid red"
-        container.style.padding = "1rem"
-        container.style.display = "flex"
-        container.dataset.messageId = messageId
-        message.textContent = params.join("")
-        btn.textContent = "mark as read"
-
-        container.appendChild(message)
-        const containerClone = container.cloneNode(true)
-        container.appendChild(btn)
-        inbox1.appendChild(container)
-        read2.appendChild(containerClone)
+  fromEvent(btn, 'click').pipe(
+    first()
+  ).subscribe(_ => read$.next(messageId))
 })
 
-Observable.fromEvent(dqs("#clear"), "click").subscribe(() => {
-        inbox1.innerHTML = ""
-        inbox2.innerHTML = ""
-        read1.innerHTML = ""
-        read2.innerHTML = ""
+fromEvent(dqs('#clear'), 'click').subscribe(() => {
+  inbox1.innerHTML = ''
+  inbox2.innerHTML = ''
+  read1.innerHTML = ''
+  read2.innerHTML = ''
 })
 
 /**
@@ -111,8 +111,10 @@ Observable.fromEvent(dqs("#clear"), "click").subscribe(() => {
  *      We can create event producers and each of them can
  *      Send a new message to the event bus whenever we choose
  */
-read$.map(id => dqsa(`[data-message-id="${id}"]`)).subscribe(nodeList => {
-        nodeList.forEach(params => {
-                params.style.border = ""
-        })
-})
+read$.pipe(
+  map(id => dqsa(`[data-message-id="${id}"]`))
+).subscribe(nodeList => {
+    nodeList.forEach(params => {
+      params.style.border = ''
+    })
+  })
--- examples/http/scripts/utils.js
+++ examples/http/scripts/utils.js
@@ -1,6 +1,9 @@
 export const dqs = selector => document.querySelector(selector)
 export const dqsa = selector => document.querySelectorAll(selector)
 export const dce = el => document.createElement(el)
+export const div = () => dce('div');
+export const p = () => dce('p');
+export const button = () => dce('button');
 
 export const observer = id => ({
   next: val => console.log(`${id} [ next ] ${val}`),
