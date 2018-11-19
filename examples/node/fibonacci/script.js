384ff1c60f47dea67ccb1db7f903f5e90d6e0bd9
jsonberry <jason.s.awbrey@gmail.com>
2018-11-18 16:22:45 -0800

    chore: upate fibonacci with pipes

--- examples/node/fibonacci/script.js
+++ examples/node/fibonacci/script.js
@@ -1,20 +1,12 @@
-const { Observable } = require("rxjs")
-const { scan, startWith, take, map } = require("rxjs/operators")
+const { interval } = require('rxjs')
+const { scan, take } = require('rxjs/operators')
 
-const initialConditions = { term1: 0, term0: 1 }
-const iterations = 10
-const sequencer = acc => ({
-        term1: acc.term0,
-        term0: acc.term0 + acc.term1,
-})
-const fibonacciSequencer = scan(sequencer, initialConditions)
-const fibonacciNumber = map(({term0, term1}) => term0 + term1)
-
-Observable.interval(250)
-        .pipe(
-            fibonacciSequencer,
-            startWith(initialConditions),
-            fibonacciNumber,
-            take(iterations),
-        )
-        .subscribe(num => console.log(`Fibonacci: ${num}`))
+interval(10)
+  .pipe(
+    scan(acc => ({
+      next: acc.next + acc.previous,
+      previous: acc.next,
+    }), { previous: 1, next: 0 }),
+    take(20), // just so we don't get to Infinity
+  )
+  .subscribe(num => console.log(`Fibonacci: ${num.previous}`))
