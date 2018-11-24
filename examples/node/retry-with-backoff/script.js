cfcd90cf62cb977f5dc6939ad70e01ea576639dc
jsonberry <jason.s.awbrey@gmail.com>
2018-11-23 21:33:59 -0800

    chore(examples): updated retry-with-backoff to use pipe operators

--- examples/node/retry-with-backoff/script.js
+++ examples/node/retry-with-backoff/script.js
@@ -1,5 +1,13 @@
-const { Observable, defer, of, range, timer, timeout, zip } = require('rxjs')
-const { catchError, map, mergeMap, retryWhen } = require('rxjs/operators')
+const { defer, EMPTY, of, range, throwError, timer } = require('rxjs')
+const {
+  catchError,
+  map,
+  mergeMap,
+  retryWhen,
+  timeout,
+  zip,
+} = require('rxjs/operators')
+const { observer } = require('../utils')
 
 let foo = null
 
@@ -8,36 +16,36 @@ setTimeout(() => {
 }, 5000)
 
 const retryWithBackoff = ({
-  fallback = Observable.empty(),
+  fallback = EMPTY,
   retries = 5,
   strategy = 'linear',
-  timeout = 15000,
+  _timeout = 15000,
 }) => stream$ =>
   stream$.pipe(
     retryWhen(errors$ =>
-      errors$
-        .timeout(timeout)
-        .zip(Observable.range(1, retries + 1))
-        .pipe(
-          mergeMap(([error, i]) => {
-            if (i > retries) {
-              return Observable.throw(error)
-            }
+      errors$.pipe(
+        timeout(_timeout),
+        zip(range(1, retries + 1)),
+        mergeMap(([error, attempt]) => {
+          console.log(error, attempt, retries);
+          if (attempt > retries) {
+            return throwError(error)
+          }
 
-            switch (strategy) {
-              case 'linear':
-                return Observable.timer(i * 1000)
+          switch (strategy) {
+            case 'linear':
+              return timer(attempt * 1000)
 
-              case 'exponential':
-                return Observable.timer(Math.pow(2.71828, i))
-            }
-          }),
-        ),
+            case 'exponential':
+              return timer(Math.pow(Math.E, attempt))
+          }
+        }),
+      ),
     ),
     catchError(_ => fallback),
   )
 
-Observable.defer(() => Observable.of(foo))
+defer(() => of(foo))
   .pipe(
     map(ev => {
       if (!ev) {
@@ -47,10 +55,10 @@ Observable.defer(() => Observable.of(foo))
       }
     }),
     retryWithBackoff({
-      fallback: Observable.of('I am a fallback'),
+      fallback: of('I am a fallback'),
       retries: 10,
       strategy: 'exponential',
-      timeout: 3000, // Try increasing or removing this timeout to see "foo" come through, instead of the fallback
+      _timeout: 3000, // Try increasing or removing this timeout to see "foo" come through, instead of the fallback
     }),
   )
-  .subscribe(ev => console.log(ev))
+  .subscribe(observer('retryWithBackoff'))
