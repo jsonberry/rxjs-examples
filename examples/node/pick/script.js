f26e7baf9bc0ccebf60dea084b2fd152e9d5ac6d
jsonberry <jason.s.awbrey@gmail.com>
2018-11-23 20:48:09 -0800

    feat(pick): add pick example

--- examples/node/pick/script.js
+++ examples/node/pick/script.js
@@ -1,10 +1,13 @@
 const { of } = require('rxjs')
 const { pick } = require('rxjs-toolkit')
 
+/**
+ * Pick is a customer operator that is using Lodash pick under the hood
+ */
 of({
   foo: 'foo',
   bar: 'bar',
   baz: 'baz',
-}).pipe(
-  pick('foo', 'baz'),
-).subscribe(x => console.log(x))
+})
+  .pipe(pick('foo', 'baz'))
+  .subscribe(x => console.log(x))
