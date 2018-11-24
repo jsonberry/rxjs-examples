866b34e36b542e7fc9b6741695c919dd50527178
jsonberry <jason.s.awbrey@gmail.com>
2018-11-23 22:21:30 -0800

    chore(examples): update share and cache stream with pipeable operators

--- examples/node/share-and-cache-stream/script.js
+++ examples/node/share-and-cache-stream/script.js
@@ -1,48 +1,61 @@
-const { Observable, timer } = require("rxjs")
-
 const axios = require('axios')
+const { of, from, interval } = require('rxjs')
+const {
+  publish,
+  publishReplay,
+  refCount,
+  share,
+  shareReplay,
+  take,
+  tap,
+  pluck,
+  filter,
+  mergeMap,
+} = require('rxjs/operators')
+const { observer } = require('../utils')
 
-const { observer } = require("../utils")
 
-// const event = Observable.interval(500).take(3)
-const event = Observable.of("event")
-        .do(() => console.log("[ side effect ]"))
-        // .share()
-        // .publish()
-        // .publish().refCount()
-        // .publishReplay()
-        // .publishReplay().refCount()
-        // .shareReplay()
+
+// const event = interval(500).pipe(take(3),
+const event = of('event').pipe(
+  tap(() => console.log('[ side effect ]')),
+  // share(),
+  // publish(),
+  // publishReplay(),
+  // refCount(), // play with this versus the `connect` below in the setTimeout
+  // shareReplay(),
+)
 
 // event.subscribe(observer("a"))
 // event.subscribe(observer("b"))
 
 // Required for the publish() and publishReplay() operators
 setTimeout(() => {
-//     event.connect()
-}, 50);
+  //     event.connect()
+}, 50)
 
 setTimeout(() => {
-//         event.subscribe(observer("c"))
+  //         event.subscribe(observer("c"))
 }, 1000)
-
 
 /**
  *      Caching and Sharing of XHR Request
  *      Remove the shareReplay to see multiple XHR requests being made
  *      Add it back on to see only one request made
  */
+const samplePage$ = from(
+  axios.get('https://ey-intuitive.com/wp-json/wp/v2/pages'),
+).pipe(
+  tap(() => console.log('[ requesting data from source ]')),
+  mergeMap(({ data }) => data),
+  filter(({ slug }) => slug === 'careers'),
+  pluck('title', 'rendered'),
+  shareReplay(),
+)
 
-const samplePage$ = Observable.from(axios.get('http://demo.wp-api.org/wp-json/wp/v2/pages'))
-        .do(() => console.log('[ requesting data from source ]'))
-        .mergeMap(({data}) => data)
-        .filter(({slug}) => slug === 'sample-page')
-        .map(page => page.title.rendered)
-        .shareReplay(1)
-
-// samplePage$.subscribe(observer("a"))
-// samplePage$.subscribe(observer("b"))
+samplePage$.subscribe(observer("a"))
+samplePage$.subscribe(observer("b"))
 
 setTimeout(() => {
-        // samplePage$.subscribe(observer("c"))
+  samplePage$.subscribe(observer("c"))
 }, 1000)
